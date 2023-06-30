927ce0bbfaf9b3f5c89e4a2a036ac0d4e97f2c3c
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-06-30 18:41:30 -0300

    fix: error in mongodb

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,13 +1,25 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, type Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__)
+    this.client = await MongoClient.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
   },
 
   async disconnect (): Promise<void> {
-    await this.client.close
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().Collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
   }
 }
