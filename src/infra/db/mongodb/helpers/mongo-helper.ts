828f61be5d8db1b849933f0fdf0ae6a74d86aa20
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-06-30 21:54:32 -0300

    feat: connect to mongodb before app listen

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,7 +4,7 @@ export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URI, {
+    this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
@@ -15,7 +15,7 @@ export const MongoHelper = {
   },
 
   getCollection (name: string): Collection {
-    return this.client.db().Collection(name)
+    return this.client.db().collection(name)
   },
 
   map (collection: any): any {
