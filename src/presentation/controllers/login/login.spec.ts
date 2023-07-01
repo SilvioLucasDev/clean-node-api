3a913a23ecf02d63268b1d171a956cd12d4eee95
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-01 19:22:19 -0300

    refactor: group LoginCotroller dependencies in a single file

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,6 @@
-import { type Authentication } from '../../../domain/usecases/authentication'
+import { type HttpRequest, type EmailValidator, type Authentication } from './login-protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
-import { type HttpRequest, type EmailValidator } from '../signup/signup-protocols'
 import { LoginController } from './login'
 
 const makeEmailValidator = (): EmailValidator => {
