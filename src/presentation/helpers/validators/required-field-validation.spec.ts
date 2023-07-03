cd668172dca4994f98fb101ddf82fcc35369bc29
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-03 15:17:35 -0300

    test: move sut creation to a factory method

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,9 +2,13 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
 describe('Required Field Validation', () => {
   test('Should return an MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({
       email: 'any_email@email.com'
     })
@@ -12,7 +16,7 @@ describe('Required Field Validation', () => {
   })
 
   test('Should not return if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({
       field: 'any_email@email.com'
     })
