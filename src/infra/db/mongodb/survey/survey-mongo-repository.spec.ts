3d99312a9a7a6e325a473e7dda8efd7e56b6dc62
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-13 14:42:12 -0300

    test: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -72,4 +72,21 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const survey = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      })
+
+      const sut = makeSut()
+      const surveyResult = await sut.loadById(survey.insertedId.toString())
+      expect(surveyResult).toBeTruthy()
+    })
+  })
 })
