d9970f65c27142817feee19606da120ff4089fc8
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-15 02:09:48 -0300

    refactor: use builder patterns to build the aggregation query

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,9 +1,9 @@
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { type Collection } from 'mongodb'
 import { type SurveyModel } from '@/domain/models/survey'
 import { type AccountModel } from '@/domain/models/account'
 import { mockAddAccountParams } from '@/domain/test'
+import { ObjectId, type Collection } from 'mongodb'
 
 let surveyResultCollection: Collection
 let surveyCollection: Collection
@@ -26,14 +26,14 @@ const makeSurvey = async (): Promise<SurveyModel> => {
   })
 
   const surveyCreated: any = await surveyCollection.findOne({ _id: survey.insertedId })
-  return surveyCreated
+  return MongoHelper.map(surveyCreated)
 }
 
 const makeAccount = async (): Promise<AccountModel> => {
   const account = await accountCollection.insertOne(mockAddAccountParams())
 
   const accountCreated: any = await accountCollection.findOne({ _id: account.insertedId })
-  return accountCreated
+  return MongoHelper.map(accountCreated)
 }
 
 describe('Survey Result Mongo Repository', () => {
@@ -69,31 +69,35 @@ describe('Survey Result Mongo Repository', () => {
         date: new Date()
       })
       expect(surveyResult).toBeTruthy()
-      expect(surveyResult.id).toBeTruthy()
-      expect(surveyResult.answer).toBe(survey.answers[0].answer)
+      expect(surveyResult.surveyId).toEqual(survey.id)
+      expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
+      expect(surveyResult.answers[0].count).toBe(1)
+      expect(surveyResult.answers[0].percent).toBe(100)
     })
 
     test('Should update survey result if its not new', async () => {
       const survey = await makeSurvey()
       const account = await makeAccount()
 
-      const surveyResultOld = await surveyResultCollection.insertOne({
-        surveyId: survey.id,
+      await surveyResultCollection.insertOne({
+        surveyId: new ObjectId(survey.id),
         accountId: account.id,
         answer: survey.answers[0].answer,
         date: new Date()
       })
 
       const sut = makeSut()
-      const surveyResultNew = await sut.save({
+      const surveyResult = await sut.save({
         surveyId: survey.id,
         accountId: account.id,
         answer: survey.answers[1].answer,
         date: new Date()
       })
-      expect(surveyResultNew).toBeTruthy()
-      expect(surveyResultNew.id).toEqual(surveyResultOld.insertedId)
-      expect(surveyResultNew.answer).toBe(survey.answers[1].answer)
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.surveyId).toEqual(survey.id)
+      expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer)
+      expect(surveyResult.answers[0].count).toBe(1)
+      expect(surveyResult.answers[0].percent).toBe(100)
     })
   })
 })
--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,22 +1,105 @@
-import { MongoHelper } from '../helpers/mongo-helper'
-import { type SaveSurveyResultParams, type SurveyResultModel, type SaveSurveyResultRepository } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
+import { type SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
+import { MongoHelper, QueryBuilder } from '../helpers'
+import { type SurveyResultModel } from '@/domain/models/survey-result'
+import { type SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
+import { ObjectId } from 'mongodb'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
-    const surveyResult = await surveyResultCollection.findOneAndUpdate({
-      surveyId: data.surveyId,
-      accountId: data.accountId
+    await surveyResultCollection.findOneAndUpdate({
+      surveyId: new ObjectId(data.surveyId),
+      accountId: new ObjectId(data.accountId)
     }, {
       $set: {
         answer: data.answer,
         date: data.date
       }
     }, {
-      upsert: true,
-      returnDocument: 'after'
+      upsert: true
     })
+    const surveyResult = await this.loadBySurveyId(data.surveyId)
+    console.log(surveyResult)
+    return surveyResult
+  }
 
-    return surveyResult.value && MongoHelper.map(surveyResult.value)
+  private async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const query = new QueryBuilder()
+      .match({
+        surveyId: new ObjectId(surveyId)
+      })
+      .group({
+        _id: 0,
+        data: {
+          $push: '$$ROOT'
+        },
+        count: {
+          $sum: 1
+        }
+      })
+      .unwind({
+        path: '$data'
+      })
+      .lookup({
+        from: 'surveys',
+        foreignField: '_id',
+        localField: 'data.surveyId',
+        as: 'survey'
+      })
+      .unwind({
+        path: '$survey'
+      })
+      .group({
+        _id: {
+          surveyId: '$survey._id',
+          question: '$survey.question',
+          date: '$survey.date',
+          total: '$count',
+          answer: {
+            $filter: {
+              input: '$survey.answers',
+              as: 'item',
+              cond: {
+                $eq: ['$$item.answer', '$data.answer']
+              }
+            }
+          }
+        },
+        count: {
+          $sum: 1
+        }
+      })
+      .unwind({
+        path: '$_id.answer'
+      })
+      .addFields({
+        '_id.answer.count': '$count',
+        '_id.answer.percent': {
+          $multiply: [{
+            $divide: ['$count', '$_id.total']
+          }, 100]
+        }
+      })
+      .group({
+        _id: {
+          surveyId: '$_id.surveyId',
+          question: '$_id.question',
+          date: '$_id.date'
+        },
+        answers: {
+          $push: '$_id.answer'
+        }
+      })
+      .project({
+        _id: 0,
+        surveyId: '$_id.surveyId',
+        question: '$_id.question',
+        date: '$_id.date',
+        answers: '$answers'
+      })
+      .build()
+    const surveyResult: any = await surveyResultCollection.aggregate(query).toArray()
+    return surveyResult?.length ? surveyResult[0] : null
   }
 }
