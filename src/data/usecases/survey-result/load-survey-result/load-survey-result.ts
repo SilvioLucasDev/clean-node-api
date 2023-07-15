49849039a1b52f8efdf6f68f4b80c550be7cf4f4
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-07-15 18:10:10 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct value

---
+++ src/data/usecases/survey-result/load-survey-result/load-survey-result.ts
@@ -0,0 +1,11 @@
+import { type LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+import { type SurveyResultModel } from '../save-survey-result/db-save-survey-result-protocols'
+import { type LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  async load (surveyId: string): Promise<SurveyResultModel> {
+    await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    return null
+  }
+}
